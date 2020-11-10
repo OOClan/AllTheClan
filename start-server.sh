841a4a84eb340fb5ad3ea68bd78986f7eb3f36b6
root <root>
2020-11-10 18:34:19 +0200

    Update 1.3.4c

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 java -d64 -server -Xms8G -Xmx8G -XX:ParallelGCThreads=4 \
     -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC \
-    -jar forge-1.16.4-35.1.4.jar
+    -jar forge-1.16.4-35.1.13.jar
