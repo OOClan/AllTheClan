8ec8ba4838a12b6528f882d118c5e6231846ed6d
Victor <virb3e@gmail.com>
2021-01-30 21:06:16 +0000

    Update 1.5.0
    • Added CTM
    • Added Performant
    • Added Cartographer
    • Increased dragons spawns and added pixies
    • Missing Nether and End issues have been fixed with forge
    • Updated All the Mods
    • Forge Version is 36.0.9

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 java -d64 -server -Xms8G -Xmx8G -XX:ParallelGCThreads=4 \
     -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC \
-    -jar forge-1.16.4-35.1.36.jar
+    -jar forge-1.16.5-36.0.9.jar
